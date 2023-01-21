16c0a049284de13d97c338232367e4c80c8843a9
Rod97139 <fayaflame@gmail.com>
2023-01-21 13:26:34 +0100

    fixtures details

--- src/DataFixtures/LodgingFixtures.php
+++ src/DataFixtures/LodgingFixtures.php
@@ -4,10 +4,14 @@ namespace App\DataFixtures;
 
 use App\Entity\Lodging;
 use App\Repository\CityRepository;
+use App\Repository\CriteriaRepository;
 use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 // class LodgingFixtures extends Fixture implements DependentFixtureInterface
 class LodgingFixtures extends Fixture
@@ -15,53 +19,61 @@ class LodgingFixtures extends Fixture
     
     public function __construct(
         private CityRepository $cityRepository,
-        private UserRepository $userRepository
+        private UserRepository $userRepository,
+        private CriteriaRepository $criteriaRepository
     ) {}
     public function load(ObjectManager $manager): void
 
     {
+        $faker = Factory::create('fr_FR');
 
         $cities = $this->cityRepository->findAll();
         
         $users = $this->userRepository->findByRole('ROLE_OWNER');
 
-        $data =  $data = [
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg'],
-            ['Gîte de base', 'Description du gîte en question', 4, 5, 100, 100, '5 rue de la paix', '/uploads/images/gitetest-63c7a74d73062.jpg']
-       ];
+        $pictures = [
+            '/uploads/images/gite_example.jpg',
+            '/uploads/images/gite-test3.jpg',
+            '/uploads/images/gite-test4.jpg',
+            '/uploads/images/gite-test5.jpg'
+            ];
+        
+        $criteria = $this->criteriaRepository->findAll();
 
 
 
-        for ($i=0; $i < count($data); $i++) { 
+
+        for ($i=0; $i < 40 ; $i++) { 
             $lodging = new Lodging();
 
+            $picturesIndex = array_rand($pictures, 1);
             $cityIndex = array_rand($cities, 1);
             $userIndex = array_rand($users, 1);
 
             $lodging->setCity($cities[$cityIndex]);
             $lodging->setUser($users[$userIndex]);
         
-            $lodging->setName($data[$i][0]);
-            $lodging->setDescription($data[$i][1]);
-            $lodging->setNumberRooms($data[$i][2]);
-            $lodging->setMaxPeople($data[$i][3]);
-            $lodging->setSurface($data[$i][4]);
-            $lodging->setWeeklyBasePrice($data[$i][5]);
-            $lodging->setAdress($data[$i][6]);
-            $lodging->setImage($data[$i][7]);
+            $lodging->setName($faker->lexify('Gîte ????'));
+            // $lodging->setDescription($faker->text(200));
+            $lodging->setDescription('test description');
+            $nbrRooms = $faker->numberBetween(2,5); 
+            $lodging->setNumberRooms($nbrRooms);
+            $lodging->setMaxPeople($nbrRooms + 1);
+            $surface = $faker->numberBetween(50,300);
+            $lodging->setSurface($surface);
+            $lodging->setWeeklyBasePrice($surface * 1.5);
+            $lodging->setAdress($faker->streetAddress);
+            $lodging->setImage($pictures[$picturesIndex]);
             $lodging->setCreatedAt(new \DateTime());
+
+            $lodging->addCriterion($criteria[1])
+                    ->addCriterion($criteria[6])
+                    ->addCriterion($criteria[8])
+                    ->addCriterion($criteria[9])
+                    ->addCriterion($criteria[13])
+                    ->addCriterion($criteria[15])
+                    ->addCriterion($criteria[17]);
+
             $manager->persist($lodging);
         }
 
