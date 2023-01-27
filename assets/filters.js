8baaf481196033a7d72568abd3e451efda4762ca
Rod97139 <fayaflame@gmail.com>
2023-01-27 19:43:59 +0100

    webpack filters.js

--- assets/filters.js
+++ assets/filters.js
@@ -6,9 +6,26 @@ document.querySelectorAll("#filters input").forEach(input => {
         // on intercepte les clics
         // on récupère les datas
         const Form = new FormData(FiltersForm)
+
+        // on fabrique l'url
+        const Params = new URLSearchParams()
+        
         Form.forEach((value, key) => {
-            console.log(key, value);
+            Params.append(key, value)
+            console.log(Params.toString());
         })
+        //On récupere l'url active
+        const Url = new URL(window.location.href)
+        
+        // On lance la requete ajax
+        fetch(Url.origin + "/all/?" + Params.toString() + "&ajax=1", {
+            headers: {
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        }).then(response => {
+            console.log(response);
+        }).catch(e => alert(e))
+
     })
 
 })
